f41dad416bef10f6ee7d447f7b84b3bc1470f639
chuvitvang <152381244+chuvitvang@users.noreply.github.com>
2024-10-08 20:27:29 +0700

    Update kiemtramatkhau.py

--- kiemtramatkhau.py
+++ kiemtramatkhau.py
@@ -1,16 +1,16 @@
-i = 0
-while True:
-    mk_nhap = input("Nhập mật khẩu: ")
-    if mk_nhap == "1234":
+i = 0 #khời tạo biến đếm
+while True: #tạo vòng lặp vô hạn
+    mk_nhap = input("Nhập mật khẩu: ") #gán input cho mk_nhap
+    if mk_nhap == "1234": #kiểm tra mk_nhap
         print(f"đăng nhập thành công, mật khẩu là {mk_nhap}")
-        break
+        break #thoát vòng lặp
     else:
-        i += 1
+        i += 1 #tăng biến đếm
         sai = f"còn {3-i} lần nhập,"
-        if i < 3:
+        if i < 3: #kiểm tra đã sai 3 lần hay chưa
             print(sai,"vui lòng nhập lại")
         else:
             print(sai,"đột ngột vi phạm, thoát chương trình")
-            exit()
+            exit() #thoát chương trình
             
 print("tiếp tục code")
